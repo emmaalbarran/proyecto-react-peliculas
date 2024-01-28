8999b16d37bc2f6e6ab1bb2a6310984e030e993a
Emma Albarran Villasmil <ealbarran@kdu.cl>
2024-01-28 00:59:09 -0300

    githubpages

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import MovieDetail from './components/movie/MovieDetail';
 import './App.css'
 
 function App() {
-  // Obtener las películas almacenadas en localStorage o usar un array inicial
+
   const storedMovies = JSON.parse(localStorage.getItem('movies')) || [];
   const [movies, setMovies] = useState(storedMovies);
 
-  // Almacenar las películas en localStorage cada vez que cambien
+
   useEffect(() => {
     localStorage.setItem('movies', JSON.stringify(movies));
   }, [movies]);
