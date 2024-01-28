dd2c1bb6d7348511252b8444ae9dd936c98842f7
Emma Albarran Villasmil <ealbarran@kdu.cl>
2024-01-28 02:13:13 -0300

    .

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/proyecto-react-peliculas/', // Ajusta la ruta base según tu configuración
+});
